632b377234316274a34a77a12dc2f7f7b355543c
Gideon Komla Agbavor <gideonagbavor8@gmail.com>
2026-05-06 22:56:15 +0000

    Add API root view, set DEBUG=False for production, add CSRF trusted origins

--- task_management_api/settings.py
+++ task_management_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5k!270)ecz9*vrwqw9d^)f5bs0*yy+qcbfib(-nq+33nwjd5pw'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5k!270)ecz9*vrwqw9d^)f5bs0*yy+qcbfib(-nq+33nwjd5pw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.vercel.app',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
--- task_management_api/urls.py
+++ task_management_api/urls.py
@@ -1,21 +1,9 @@
 """
 URL configuration for task_management_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework.routers import SimpleRouter
 from tasks.views import TaskViewSet, UserViewSet, CategoryViewSet, UserRegistrationView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -26,12 +14,29 @@ router.register('tasks', TaskViewSet, basename='task')
 router.register('users', UserViewSet, basename='user')
 router.register('categories', CategoryViewSet, basename='category')
 
+
+def api_root(request):
+    return JsonResponse({
+        "message": "Welcome to the Task Management API",
+        "version": "1.0",
+        "endpoints": {
+            "admin": "/admin/",
+            "tasks": "/api/tasks/",
+            "categories": "/api/categories/",
+            "users": "/api/users/",
+            "register": "/register/",
+            "token": "/api/token/",
+            "token_refresh": "/api/token/refresh/",
+        }
+    })
+
+
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('', include('tasks.urls')),  # Include the tasks app's URLs
+    path('', include('tasks.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
 ]
-
